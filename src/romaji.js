d309fdaaef92537e7b73f254325fe3f5354d1ebf
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-03-21 22:56:02 +0900

    Add test case for toHiragana

--- src/romaji.js
+++ src/romaji.js
@@ -1,7 +1,6 @@
 export default class{
 
-  static add(value1, value2){
-    return value1 + value2
+  static toHiragana(text){
   }
 
 }
--- src/romaji.test.js
+++ src/romaji.test.js
@@ -1,5 +1,15 @@
 import Romaji from './romaji'
 
-it('add', () => {
-  expect(Romaji.add(1, 2)).toBe(3)
+it('toHiragana', () => {
+  expect(Romaji.toHiragana('aiueo')).toBe('あいうえお')
+  expect(Romaji.toHiragana('kakikukeko')).toBe('かきくけこ')
+  expect(Romaji.toHiragana('sasisuseso')).toBe('さしすせそ')
+  expect(Romaji.toHiragana('tatituteto')).toBe('たちつてと')
+  expect(Romaji.toHiragana('naninuneno')).toBe('なにぬねの')
+  expect(Romaji.toHiragana('hahihuheho')).toBe('はひふへほ')
+  expect(Romaji.toHiragana('mamimumemo')).toBe('まみむめも')
+  expect(Romaji.toHiragana('yayuyo')).toBe('やゆよ')
+  expect(Romaji.toHiragana('rarirurero')).toBe('らりるれろ')
+  expect(Romaji.toHiragana('wawo')).toBe('わを')
+  expect(Romaji.toHiragana('nn')).toBe('ん')
 })
