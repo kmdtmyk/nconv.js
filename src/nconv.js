199585f0f8b41c634f3924496e99df6b709e4ec4
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-10-10 11:38:20 +0900

    Add convert pattern to romajiToHiragana

--- src/nconv.js
+++ src/nconv.js
@@ -11,6 +11,7 @@ export default class{
 
     const convertMap1 = {
       a: 'あ', i: 'い', u: 'う', e: 'え', o: 'お',
+      '-': 'ー',
     }
     const convertMap2 = {
       ka: 'か', ki: 'き', ku: 'く', ke: 'け', ko: 'こ',
@@ -19,12 +20,44 @@ export default class{
       na: 'な', ni: 'に', nu: 'ぬ', ne: 'ね', no: 'の',
       ha: 'は', hi: 'ひ', hu: 'ふ', he: 'へ', ho: 'ほ',
       ma: 'ま', mi: 'み', mu: 'む', me: 'め', mo: 'も',
-      ya: 'や', yu: 'ゆ', yo: 'よ',
+      ya: 'や', yu: 'ゆ', ye: 'いぇ', yo: 'よ',
       ra: 'ら', ri: 'り', ru: 'る', re: 'れ', ro: 'ろ',
       wa: 'わ', wo: 'を',
       nn: 'ん',
+      ga: 'が', gi: 'ぎ', gu: 'ぐ', ge: 'げ', go: 'ご',
+      za: 'ざ', zi: 'じ', zu: 'ず', ze: 'ぜ', zo: 'ぞ',
+      da: 'だ', di: 'ぢ', du: 'づ', de: 'で', do: 'ど',
+      ba: 'ば', bi: 'び', bu: 'ぶ', be: 'べ', bo: 'ぼ',
+      pa: 'ぱ', pi: 'ぴ', pu: 'ぷ', pe: 'ぺ', po: 'ぽ',
+      fa: 'ふぁ', fi: 'ふぃ', fu: 'ふ', fe: 'ふぇ', fo: 'ふぉ',
+      qa: 'くぁ', qi: 'くぃ', qu: 'く', qe: 'くぇ', qo: 'くぉ',
+      ja: 'じゃ', ji: 'じ', ju: 'じゅ', je: 'じぇ', jo: 'じょ',
+      va: 'ゔぁ', vi: 'ゔぃ', vu: 'ゔ', ve: 'ゔぇ', vo: 'ゔぉ',
+      xa: 'ぁ', xi: 'ぃ', xu: 'ぅ', xe: 'ぇ', xo: 'ぉ',
+      la: 'ぁ', li: 'ぃ', lu: 'ぅ', le: 'ぇ', lo: 'ぉ',
+    }
+    const convertMap3 = {
+      kya: 'きゃ', kyi: 'きぃ', kyu: 'きゅ', kye: 'きぇ', kyo: 'きょ',
+      gya: 'ぎゃ', gyi: 'ぎぃ', gyu: 'ぎゅ', gye: 'ぎぇ', gyo: 'ぎょ',
+      sya: 'しゃ', syi: 'しぃ', syu: 'しゅ', sye: 'しぇ', syo: 'しょ',
+      zya: 'じゃ', zyi: 'じぃ', zyu: 'じゅ', zye: 'じぇ', zyo: 'じょ',
+      jya: 'じゃ', jyi: 'じぃ', jyu: 'じゅ', jye: 'じぇ', jyo: 'じょ',
+      tya: 'ちゃ', tyi: 'ちぃ', tyu: 'ちゅ', tye: 'ちぇ', tyo: 'ちょ',
+      cya: 'ちゃ', cyi: 'ちぃ', cyu: 'ちゅ', cye: 'ちぇ', cyo: 'ちょ',
+      dya: 'ぢゃ', dyi: 'ぢぃ', dyu: 'ぢゅ', dye: 'ぢぇ', dyo: 'ぢょ',
+      hya: 'ひゃ', hyi: 'ひぃ', hyu: 'ひゅ', hye: 'ひぇ', hyo: 'ひょ',
+      bya: 'びゃ', byi: 'びぃ', byu: 'びゅ', bye: 'びぇ', byo: 'びょ',
+      shi: 'し', chi: 'ち', tsu: 'つ',
+      xya: 'ゃ', xyu: 'ゅ', xyo: 'ょ',
+      lya: 'ゃ', lyu: 'ゅ', lyo: 'ょ',
+      xtu: 'っ', ltu: 'っ',
+    }
+    const convertMap4 = {
+      xtsu: 'っ', ltsu: 'っ',
     }
 
+    text = applyConvertMap(text, convertMap4)
+    text = applyConvertMap(text, convertMap3)
     text = applyConvertMap(text, convertMap2)
     text = applyConvertMap(text, convertMap1)
     return text
